6f631d9f277ea321e382fb5aae298c54f5a438fd
Martine9670 <pinnamartine@free.fr>
2026-03-02 11:41:32 +0100

    Add presence and length validations to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # On veut que le titre soit présent et fasse au moins 5 caractères
+  validates :title, presence: true, length: { minimum: 5 }
+  
+  # On veut que le corps (body) soit présent
+  validates :body, presence: true
 end
