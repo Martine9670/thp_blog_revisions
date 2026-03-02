b02e7716fd2c6ca7d17d6b648cd28d19b5e131ff
Martine9670 <pinnamartine@free.fr>
2026-03-02 13:49:24 +0100

    Reset database to incorporate user_id in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,37 @@
 class CommentsController < ApplicationController
-    # Seule la suppression demande un mot de passe
-  http_basic_authenticate_with name: "martine", password: "password", only: :destroy
-  
+  # 1. On utilise Devise pour l'auth, plus le mot de passe "martine"
+  before_action :authenticate_user!
+
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    
+    # 2. On lie le commentaire à l'article ET à l'utilisateur connecté
+    @comment = @article.comments.build(comment_params)
+    @comment.user = current_user 
+
+    if @comment.save
+      redirect_to article_path(@article), notice: "Commentaire publié !"
+    else
+      redirect_to article_path(@article), alert: "Erreur : le commentaire est vide."
+    end
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: :see_other
+
+    # 3. SÉCURITÉ CRUCIALE (Bonus 2.8.3) : Seul l'auteur peut supprimer
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to article_path(@article), notice: "Commentaire supprimé.", status: :see_other
+    else
+      redirect_to article_path(@article), alert: "Action interdite : ce n'est pas ton commentaire !"
+    end
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      # 4. On ne permet plus :commenter (il n'existe plus en DB)
+      params.require(:comment).permit(:body)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_122210) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_02_124032) do
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -27,7 +27,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_122210) do
     t.string "commenter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_122210) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
